34fdf1398192d94814d3fc5d4952dcb027d1fbae
MValderrama <mavalderrama@gmail.com>
2020-04-03 19:34:34 -0500

    sqlite updated

--- comacow/polls/models.py
+++ comacow/polls/models.py
@@ -2,8 +2,6 @@ import uuid
 from django.db import models
 from django.contrib.auth.models import User
 
-from .managers import UserManager
-
 
 class Users(models.Model):
     USER_TYPES = [
@@ -21,8 +19,6 @@ class Users(models.Model):
     phone = models.CharField(max_length=200)
     date_created = models.DateField(auto_now_add=True)
 
-    objects = UserManager()
-
     def get_full_name(self):
         '''
         Returns the first_name plus the last_name, with a space in between.
@@ -36,35 +32,23 @@ class Users(models.Model):
         '''
         return self.first_name
 
-    def email_user(self, subject, message, from_email=None, **kwargs):
-        '''
-        Sends an email to this User.
-        '''
-        send_mail(subject, message, from_email, [self.email], **kwargs)
+    # def email_user(self, subject, message, from_email=None, **kwargs):
+    #     '''
+    #     Sends an email to this User.
+    #     '''
+    #     send_mail(subject, message, from_email, [self.email], **kwargs)
 
 
-@receiver(post_save, sender=User)
-def create_user_profile(sender, instance, created, **kwargs):
-    if created:
-        Users.objects.create(user=instance)
+# @receiver(post_save, sender=User)
+# def create_user_profile(sender, instance, created, **kwargs):
+#     if created:
+#         Users.objects.create(user=instance)
+#
+#
+# @receiver(post_save, sender=User)
+# def save_user_profile(sender, instance, **kwargs):
+#     instance.profile.save()
 
-
-@receiver(post_save, sender=User)
-def save_user_profile(sender, instance, **kwargs):
-    instance.profile.save()
-
-
-class FarmOrder(models.Model):
-    STATUS = [
-        ("FS", "For Sale"),
-        ("SD", "Sold"),
-        ("NA", "Not Available")
-    ]
-    id_order = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    id_user = models.ForeignKey(User, on_delete=models.CASCADE)
-    id_animal = models.ForeignKey(Livestock, on_delete=models.CASCADE)
-    status = models.CharField(default=2, choices=STATUS)
-    details = models.CharField(default=500)
 
 
 class Farm(models.Model):
@@ -102,6 +86,19 @@ class Livestock(models.Model):
 
     def __str__(self):
         return self.chapeta
+
+
+class FarmOrder(models.Model):
+    STATUS = [
+        ("FS", "For Sale"),
+        ("SD", "Sold"),
+        ("NA", "Not Available")
+    ]
+    id_order = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    id_user = models.ForeignKey(User, on_delete=models.CASCADE)
+    id_animal = models.ForeignKey(Livestock, on_delete=models.CASCADE)
+    status = models.CharField(max_length=2, choices=STATUS)
+    details = models.CharField(max_length=500)
 
 
 class MiddlemanOrder(models.Model):
